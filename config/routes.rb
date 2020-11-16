4a642ec96b0a447181e8f111a221bda0f2e3f84a
Romeo de Wit <romeo@classicremedies.co.uk>
2020-11-16 15:45:14 +0000

    base done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :services do
+    resources :bookings, only: [:create]
+  end
+  resources :bookings, only: [:index, :show, :edit, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
