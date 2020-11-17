baf6aeb023c5611c5c70389decb3a355f4056bb3
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2020-11-17 14:13:31 +0000

    Cloudinary setup completed with model adjusted and photos added to params

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,10 @@
 class Service < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many_attached :photos
+
+  CATEGORIES = ['Front-End Development', 'Back-End Development', 'Full-Stack Development']
+  validates :title, :description, :category, :price, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
+
 end
