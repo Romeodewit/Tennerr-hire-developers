25026a967458bc06a5bad80caaca56a91ad9c3ea
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2020-11-17 11:43:52 +0000

    Service validations added

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,10 @@
 class Service < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  CATEGORIES = ['Front-End Development', 'Back-End Development', 'Full-Stack Development']
+  validates :title, :description, :category, :price, presence: true
+  validates :user_id, presence: true, uniqueness: true
+  validates :category, inclusion: { in: CATEGORIES }
+
 end
