316eaba96b4306962fa3072ed477d44cd4b0df7f
rafik mrabet <mrabetr@hotmail.com>
2020-11-20 13:42:15 +0000

    updated home page

--- app/javascript/plugins/home_banner.js
+++ app/javascript/plugins/home_banner.js
@@ -2,8 +2,8 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Find the perfect web app development services", "Explore our services"],
-    typeSpeed: 50,
+    strings: ["Need an online presence?", "Explore our services!"],
+    typeSpeed: 70,
     loop: true
   });
 }
